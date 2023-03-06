17143f05d412c5ed5a95fe0b8ae4f4f11fff7553
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-03-07 00:37:19 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery')
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup (items){
+    const markup = items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>`;
+    }).join('');
+
+    return markup;
+} 
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData : "alt",
+    captionDelay : 250
+});
+
+gallery.on('show.simplelightbox', function () {
+});
+
+// console.log(gallery)
